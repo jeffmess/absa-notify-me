6cc8c3959ae176699aa3e3ec905925c28bc163ad
Jeffrey van Aswegen <jeffmess@gmail.com>
2012-06-18 10:25:47 +0200

    add a recursive function to convert nested hashes keys into down cased symbols

--- lib/absa-notify-me/xml_statement.rb
+++ lib/absa-notify-me/xml_statement.rb
@@ -3,6 +3,32 @@ module Absa
     
     class XmlStatement
       
+      def self.convert_hash(hash)
+        # Recursive function to convert a nested hashes keys to
+        # downcased sym links. Caters for values being Arrays of
+        # Hashes.
+        
+        new_hash = {}
+        hash.each do |k,v|
+          new_hash.merge!({k.downcase.to_sym => v}) unless (v.is_a? Array) or (v.is_a? Hash)
+
+          if v.is_a? Array
+            v.each do |h|
+              if new_hash.has_key? k.downcase.to_sym
+                new_hash[k.downcase.to_sym] << convert_hash(h)
+              else
+                new_hash.merge!({k.downcase.to_sym => [convert_hash(h)]})
+              end
+            end
+          end
+
+          if v.is_a? Hash
+            new_hash.merge!({k.downcase.to_sym => convert_hash(v)})
+          end
+        end
+        new_hash
+      end
+      
       def self.file_to_hash(file)
         input_string = File.open(file, "rb").read
         self.string_to_hash(input_string)
@@ -13,50 +39,58 @@ module Absa
         
         Nori.parser = :nokogiri
         xml_doc = Nori.parse(string)
-        
-        xml_doc.each do |k,v|
+        converted_xml_doc = self.convert_hash(xml_doc)
+                
+        converted_xml_doc.each do |k,v|
           v.each do |key, value|
-            if key == "NOTIF_HEADER"
+            if key == :notif_header
               hash[:data][:data] << {
                 type: 'header',
                 data: {
-                  daily_no: value["DAILY_NO"],
-                  client_code: value["CLIENT_ID"],
-                  client_name: value["SNAME"],
-                  processing_date: value["CREATION_DATE"],
-                  processing_time: value["CREATION_TIME"],
-                  buss_dir_code: value["BUSS_DIR_CODE"]
+                  daily_no: value[:daily_no],
+                  client_code: value[:client_id],
+                  client_name: value[:sname],
+                  processing_date: value[:creation_date],
+                  processing_time: value[:creation_time],
+                  buss_dir_code: value[:buss_dir_code]
                 }
               }
             end
-            if key == "DETAILS"
-              value["TRANSACTION"].each do |transaction|
+            if key == :details
+              transactions = value[:transaction].blank? ? value["TRANSACTION"] : value[:transaction]
+              transactions.each do |transaction|
+                amount = (transaction[:amt].to_f * 100).to_i.to_s
+                account_balance = (transaction[:acc_bal].to_f * 100).to_i.to_s
+                
                 hash[:data][:data] << {
                   type: "detail",
                   data: {
-                    account_number: transaction["TRG_ACC"],
-                    event_number: transaction["EVENT_NO"],
-                    customer_reference: transaction["CLREF"],
-                    currency: transaction["CURR"],
-                    amount: (transaction["AMT"].to_f * 100).to_i.to_s,
-                    account_balance_after_transaction: (transaction["ACC_BAL"].to_f * 100).to_i.to_s,
-                    transaction_type: transaction["TRAN_TYPE"],
-                    transaction_processing_date: transaction["PDATE"],
-                    clearance_payment_indicator: transaction["CLR_PAYM_IND"],
-                    transaction_description: transaction["PAYM_DESC"],
-                    checksum: transaction["CHECKSUM"]
+                    account_number: transaction[:trg_acc],
+                    event_number: transaction[:event_no],
+                    customer_reference: transaction[:clref],
+                    currency: transaction[:curr],
+                    amount: amount,
+                    account_balance_after_transaction: account_balance,
+                    transaction_type: transaction[:tran_type],
+                    transaction_processing_date: transaction[:pdate],
+                    clearance_payment_indicator: transaction[:clr_paym_ind],
+                    transaction_description: transaction[:paym_desc],
+                    checksum: transaction[:checksum]
                   }
                 }
               end
             end
-            if key == "NOTIF_TRAILER"
+            if key == :notif_trailer
+              total_credit = (value[:total_cr].to_f * 100).to_i.to_s
+              total_debit = (value[:total_dt].to_f * 100).to_i.to_s
+              
               hash[:data][:data] << {
                 type: "trailer",
                 data: {
-                  total_credit: (value["TOTAL_CR"].to_f * 100).to_i.to_s,
-                  total_debit: (value ["TOTAL_DT"].to_f * 100).to_i.to_s,
-                  total_recs: value["TOTAL_RECS"],
-                  check_sum: value["CHECK_SUM"]
+                  total_credit: total_credit,
+                  total_debit: total_debit,
+                  total_recs: value[:total_recs],
+                  check_sum: value[:check_sum]
                 }
               }
             end
